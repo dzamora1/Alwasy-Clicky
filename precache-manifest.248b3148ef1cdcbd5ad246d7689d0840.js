d9e653d7156bae0ac3f5fc87e5f9cad200d72acd
Royals Central <danny.zamora@gmail.com>
2018-10-29 17:10:20 -0500

    Updates

---
+++ precache-manifest.248b3148ef1cdcbd5ad246d7689d0840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42509bfea9a9cd69bbc5730b194d715d",
+    "url": "/static/media/679567.42509bfe.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a50ebae25b21876d9bc",
+    "url": "/static/js/main.8a50ebae.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "8a50ebae25b21876d9bc",
+    "url": "/static/css/main.c516b1ce.chunk.css"
+  },
+  {
+    "revision": "16bf791a1db43c5ac8ec5f3b2cd8a0e5",
+    "url": "/index.html"
+  }
+];
